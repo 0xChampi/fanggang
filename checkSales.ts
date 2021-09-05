93ccb0a3d3827735b9e7debb77ca19c10bd74d46
0xChampi <artamaryan002@gmail.com>
2021-09-05 11:57:35 -0400

    image

--- checkSales.ts
+++ checkSales.ts
@@ -25,7 +25,7 @@ const buildMessage = (sale: any) => (
 	.setColor('#0099ff')
 	.setTitle(sale.asset.name + ' has joined a new gang!')
 	.setURL(sale.asset.permalink)
-	.setAuthor('OpenFang Bot', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png', 'https://cdn.discordapp.com/attachments/882377109102809108/883999332200751104/Fangster_BotSales.jpg')
+	.setAuthor('OpenFang Bot', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png', 'https://opensea.io/collection/fanggangnft')
 	.setThumbnail(sale.asset.collection.image_url)
 	.addFields(
 		{ name: 'Name', value: sale.asset.name },
@@ -33,7 +33,7 @@ const buildMessage = (sale: any) => (
 		{ name: 'Buyer', value: sale?.winner_account?.address, },
 		{ name: 'Seller', value: sale?.seller?.address,  },
 	)
-  .setImage(sale.asset.image_url)
+  .setImage("https://cdn.discordapp.com/attachments/882377109102809108/883999332200751104/Fangster_BotSales.jpg")
 	.setTimestamp(Date.parse(`${sale?.created_date}Z`))
 	.setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
 )
